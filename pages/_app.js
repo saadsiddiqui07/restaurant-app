9fba2654dc5f0089d290044891bb4dd3c677ae23
Saad Siddiqui <saadsiddiqui927@gmail.com>
2022-02-08 16:43:50 +0530

    Add client side emotion cache

--- pages/_app.js
+++ pages/_app.js
@@ -10,7 +10,11 @@ import theme from "../src/theme";
 // Client-side cache, shared for the whole session of the user in the browser.
 const clientSideEmotionCache = createEmotionCache();
 
-function MyApp({ Component, pageProps }) {
+function MyApp({
+  Component,
+  pageProps,
+  emotionCache = clientSideEmotionCache,
+}) {
   return (
     <CacheProvider value={emotionCache}>
       <Head>

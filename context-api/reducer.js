2a453a52c05c05cfaa3047b20ba4d9c289475956
Saad Siddiqui <saadsiddiqui927@gmail.com>
2022-02-09 20:05:38 +0530

    Add new reducers

--- context-api/reducer.js
+++ context-api/reducer.js
@@ -1,16 +1,88 @@
 // initial state values of the application
 export const initialState = {
-  cart: ["saad", "siddiqui"],
+  cart: [],
   user: null,
+  total: 0,
 };
 
 const reducer = (state, action) => {
+  console.log(action);
   switch (action.type) {
     case "SET_USER":
       return {
         ...state,
         user: action.user,
       };
+    case "ADD_TO_CART":
+      return {
+        ...state,
+        cart: [...state.cart, action.item],
+      };
+    // INCREASE THE AMOUNT OF THE PRODUCT
+    case INCREASE:
+      // looping through the cart
+      let tempCart = state.cart.map((cartItem) => {
+        // checking the cart product id to the action payload id
+        if (cartItem.id === action.payload.id) {
+          // if it matches then increase the amount of the product through the matching id
+          cartItem = { ...cartItem, amount: cartItem.amount + 1 };
+        }
+        // then return the product with the newest amount
+        return cartItem;
+      });
+      return {
+        ...state,
+        cart: tempCart,
+      };
+
+    // DECREASE THE AMOUNT OF THE PRODUCT
+    case DECREASE:
+      let newCart = [];
+      // when the amount will be less than 1 then remove that item
+      if (action.payload.amount === 1) {
+        newCart = state.cart.filter(
+          (cartItem) => cartItem.id !== action.payload.id
+        );
+      } else {
+        newCart = state.cart.map((cartItem) => {
+          // checking the cart product id to the action payload id
+          if (cartItem.id === action.payload.id) {
+            // if it matches then decrease the amount of the product through the matching id
+            cartItem = { ...cartItem, amount: cartItem.amount - 1 };
+          }
+          // then return the product with the newest amount
+          return cartItem;
+        });
+      }
+      return {
+        ...state,
+        cart: newCart,
+      };
+
+    // GETTING CART TOTAL
+    case GET_TOTAL:
+      let { total, amount } = state.cart.reduce(
+        (cartTotal, cartItem) => {
+          const { price, amount } = cartItem;
+          const itemTotal = price * amount;
+
+          cartTotal.total += itemTotal;
+          cartTotal.amount += amount;
+
+          return cartTotal;
+        },
+        {
+          total: 0,
+          amount: 0,
+        }
+      );
+      total = parseFloat(total.toFixed(2));
+      return {
+        ...state,
+        total,
+        amount,
+      };
+
     default:
       return state;
   }

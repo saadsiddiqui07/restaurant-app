ab0a73af8783cca6d6cf1567c5b824bbd2ace08b
Saad Siddiqui <saadsiddiqui927@gmail.com>
2022-02-16 22:54:28 +0530

    Code refactor

--- components/Cart/Cart.js
+++ components/Cart/Cart.js
@@ -1,33 +1,19 @@
-import { useState, useEffect } from "react";
 import { useStateValue } from "../../context-api/StateProvider";
 import CheckoutItem from "../../components/CheckoutItem/CheckoutItem";
 import IconButton from "@mui/material/IconButton";
 import ArrowBackIcon from "@mui/icons-material/ArrowBack";
 import Subtotal from "../Subtotal/Subtotal";
-import { addDoc, collection, serverTimestamp } from "firebase/firestore";
-import { db } from "../../Firebase/firebase";
 
 const Cart = () => {
-  const [{ cart, total, user }, dispatch] = useStateValue();
+  const [{ cart }, dispatch] = useStateValue();
 
+  // close the drawer
   const closeDrawer = () => {
     dispatch({
       type: "CLOSE_DRAWER",
       payload: {
         isDrawerOpen: false,
       },
-    });
-  };
-
-  const addAllItems = async (e) => {
-    e.preventDefault();
-    await addDoc(collection(db, "orders"), {
-      username: user?.displayName,
-      email: user?.email,
-      items: [...cart],
-      status: false,
-      totalPay: total,
-      timestamp: serverTimestamp(),
     });
   };
 
@@ -52,7 +38,7 @@ const Cart = () => {
         />
       ))}
       <div className="p-2 w-full">
-        <Subtotal addAllItems={addAllItems} />
+        <Subtotal />
       </div>
     </div>
   );

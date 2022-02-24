a6f37e43a45e2e85aa0c10f1afcfb50810fe6d93
Saad Siddiqui <saadsiddiqui927@gmail.com>
2022-02-24 13:19:24 +0530

    Initial commit for new page

---
+++ pages/available/index.js
@@ -0,0 +1,12 @@
+import Header from "../../components/Header/Header";
+
+const Available = () => {
+  return (
+    <div className="h-screen flex flex-col">
+      <Header />
+      <div className=""></div>
+    </div>
+  );
+};
+
+export default Available;

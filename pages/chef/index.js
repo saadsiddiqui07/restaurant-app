0e3f1e78279ca235b6518b97b28fbaf1ff8200af
Saad Siddiqui <saadsiddiqui927@gmail.com>
2022-04-02 00:47:26 +0530

    Change design of the page

--- pages/chef/index.js
+++ pages/chef/index.js
@@ -1,11 +1,11 @@
 import Head from "next/head";
-import Image from "next/image";
 import { useRouter } from "next/router";
 import { useStateValue } from "../../context-api/StateProvider";
-import { Button, Box, Typography, Container } from "@mui/material";
+import { Button, Box, Container } from "@mui/material";
 import GoogleIcon from "@mui/icons-material/Google";
-import { signInWithPopup } from "firebase/auth";
+import { signInWithPopup, createUserWithEmailAndPassword } from "firebase/auth";
 import { auth, provider } from "../../Firebase/firebase";
+import RestaurantIcon from "@mui/icons-material/Restaurant";
 
 const Chef = () => {
   const [{}, dispatch] = useStateValue();
@@ -26,61 +26,69 @@ const Chef = () => {
       .catch((err) => console.log(err));
   };
 
+  const createUserWithEmail = (e) => {
+    e.preventDefault();
+  };
+
   return (
-    <div className="flex w-[100%] h-[100vh] items-center bg-gradient-to-r from-pink-300 via-purple-300 to-indigo-400">
+    <div className="flex w-[100%] h-[100vh] items-center ">
       <Head>
         <title>Login as a Chef</title>
       </Head>
+
       <Container
-        className="p-2 h-[100vh]  w-[100%] flex-col hidden md:inline"
+        className=" h-[100vh] w-[100%]  p-4 bg-[#2FAA96]  flex-col hidden md:inline"
         component="main"
-        maxWidth="xs"
       >
-        <Box className="grid h-[100%] gap-2 content-center">
-          <h1 className="font-serief text-white font-bold text-[25px] text-gray-500">
-            Hungerz Restaurant
-          </h1>
-          <Image
-            src="https://resizer.otstatic.com/v2/photos/xlarge/1/25913892.jpg"
-            alt="testing"
-            height="100%"
-            width="100%"
-            layout="responsive"
-            objectFit="contain"
-            className="rounded-lg shadow-md"
-          />
+        <Box className="grid h-[100%] justify-center text-white gap-2 content-center">
+          <div className="flex flex-col italic items-center">
+            <RestaurantIcon className="text-[90px]" />
+            <h1 className="text-center text-[40px] uppercase font-normal">
+              First we cook. <br /> then we do everything.
+            </h1>
+          </div>
         </Box>
       </Container>
-      <Container className="p-2" component="main" maxWidth="xs">
+      <Container className="p-2 flex-col w-[100%] h-[100vh]" component="main">
         <Box
           sx={{
-            marginTop: 8,
             display: "flex",
             flexDirection: "column",
             alignItems: "center",
           }}
+          className="grid h-[100%] gap-2 content-center items-center"
         >
-          <Typography
-            className="text-white text-center"
-            component="h1"
-            variant="h4"
-          >
-            Welcome to your kitchen.
-          </Typography>
-          <Typography component="h1" className="text-white" variant="h5">
-            Sign in as a chef.
-          </Typography>
-          <Box component="form" noValidate sx={{ mt: 1 }}>
-            <hr />
-            <Button
-              type="submit"
-              fullWidth
-              className="bg-black text-white font-bold hover:bg-blue-500"
-              onClick={handleSignIn}
-            >
-              Sign In With Google <GoogleIcon className="text-lg ml-2" />
-            </Button>
-          </Box>
+          <div className="w-[100%] items-center flex flex-col justify-center">
+            <div className="flex flex-col w-[60%]">
+              <h1 className="text-[30px] text-gray-600 mr-auto">
+                Login and start cooking.
+              </h1>{" "}
+              <input
+                placeholder="Enter your email"
+                className="my-2 rounded-xl p-3 w-[100%] bg-gray-100 outline-0 border-none font-bold text-gray-500"
+              />
+              <input
+                placeholder="Enter your password"
+                className="my-2 rounded-xl p-3 w-[100%] bg-gray-100 outline-0 border-none font-bold text-gray-500"
+              />
+              <div className="flex items-center p-2 justify-between">
+                <p className="text-black font-bold text-xs underline cursor pointer ">
+                  Forgot your password?
+                </p>
+                <button className="bg-[#2FAA96] text-white rounded-md font-bold px-2 py-1 hover:bg-blue-500">
+                  Sign In
+                </button>
+              </div>
+              <hr />
+              <Button
+                className="my-3 hover:text-white hover:bg-blue-500"
+                onClick={handleSignIn}
+                endIcon={<GoogleIcon />}
+              >
+                Sign in with google
+              </Button>
+            </div>
+          </div>
         </Box>
       </Container>
     </div>
--- pages/login/index.js
+++ pages/login/index.js
@@ -1,8 +1,8 @@
 import Head from "next/head";
-import Image from "next/image";
+import FoodBankIcon from "@mui/icons-material/FoodBank";
 import { useRouter } from "next/router";
 import { useStateValue } from "../../context-api/StateProvider";
-import { Button, Box, Typography, Container } from "@mui/material";
+import { Button, Box, Container } from "@mui/material";
 import GoogleIcon from "@mui/icons-material/Google";
 import { signInWithPopup } from "firebase/auth";
 import { auth, provider } from "../../Firebase/firebase";
@@ -28,70 +28,70 @@ const Login = () => {
   };
 
   return (
-    <div className="flex w-[100%] h-[100vh] bg-gradient-to-r from-green-300 via-blue-500 to-purple-600 items-center">
+    <div className="flex w-[100%] h-[100vh]  items-center ">
       <Head>
         <title>Login</title>
       </Head>
       <Container
-        className="p-2 h-[100vh]  w-[100%] flex-col hidden md:inline"
+        className=" h-[100vh] w-[100%]  p-4 bg-[#B6CDC1]  flex-col hidden md:inline"
         component="main"
-        maxWidth="xs"
       >
-        <Box className="grid h-[100%] gap-2 content-center">
-          <h1 className="font-serief font-bold text-[25px] text-white">
-            Hungerz Restaurant
-          </h1>
-          <Image
-            src="https://resizer.otstatic.com/v2/photos/xlarge/1/25913892.jpg"
-            alt="testing"
-            height="100%"
-            width="100%"
-            layout="responsive"
-            objectFit="contain"
-            className="rounded-lg shadow-md"
-          />
+        <Box className="grid h-[100%] justify-center text-white gap-2 content-center">
+          <div className="flex flex-col italic items-center">
+            <RestaurantIcon className="text-[90px]" />
+            <h1 className="text-center text-[40px] uppercase font-normal">
+              First we eat. <br /> then we do everything.
+            </h1>
+          </div>
         </Box>
       </Container>
-      <Container className="p-2" component="main" maxWidth="xs">
+      <Container className="p-2 flex-col w-[100%] h-[100vh]" component="main">
         <Box
           sx={{
             display: "flex",
             flexDirection: "column",
             alignItems: "center",
           }}
+          className="grid h-[100%] gap-2 content-center items-center"
         >
-          <Typography
-            className="text-white text-center"
-            component="h1"
-            variant="h3"
-          >
-            Welcome
-          </Typography>
-          <Typography className="text-white" component="h1" variant="h5">
-            Sign in
-          </Typography>
-          <Box noValidate className="p-2 w-[90%]" sx={{ mt: 1 }}>
+          <div className="w-[100%] items-center flex flex-col justify-center">
+            <div className="flex flex-col w-[60%]">
+              <h1 className="text-[30px] text-gray-600 mr-auto">Login</h1>{" "}
+              <input
+                placeholder="Enter your email"
+                className="my-2 rounded-xl p-3 w-[100%] bg-gray-100 outline-0 border-none font-bold text-gray-500"
+              />
+              <input
+                placeholder="Enter your password"
+                className="my-2 rounded-xl p-3 w-[100%] bg-gray-100 outline-0 border-none font-bold text-gray-500"
+              />
+              <div className="flex items-center p-2 justify-between">
+                <p className="text-black font-bold text-xs underline cursor pointer ">
+                  Forgot your password?
+                </p>
+                <button className="bg-[#B6CDC1] text-white rounded-md font-bold px-2 py-1 hover:bg-blue-500">
+                  Sign In
+                </button>
+              </div>
+              <hr />
+              <Button
+                className="my-3 hover:text-white hover:bg-blue-500"
+                onClick={handleSignIn}
+                endIcon={<GoogleIcon />}
+              >
+                Sign in with google
+              </Button>
+            </div>
+            <h3 className="my-[10px] text-gray-400">OR</h3>
+
             <Button
-              type="submit"
-              endIcon={<RestaurantIcon />}
-              fullWidth
-              className="bg-white text-black font-bold border-2 border-black hover:bg-black hover:text-white"
               onClick={() => router.push("/chef")}
+              className="my-3 bg-[#B6CDC1] w-[60%] text-white border-solid border-1 hover:text-black"
+              endIcon={<FoodBankIcon />}
             >
-              Login as a Chef
+              Login as a chef
             </Button>
-            <hr />
-            <p className="text-center my-2 text-gray-600 ">OR</p>
-            <Button
-              type="submit"
-              fullWidth
-              className="bg-white text-black font-bold hover:bg-white hover:text-black"
-              onClick={handleSignIn}
-            >
-              Sign In With Google <GoogleIcon className="text-lg ml-2" />
-            </Button>
-            <hr />
-          </Box>
+          </div>
         </Box>
       </Container>
     </div>
